b1e44d73dbdb2dcefc70a3627bc55d2e303be713
Aminemh <mahdiamine@outlook.com>
2024-11-22 12:17:14 +0100

    had a problem with the esling config file that wasnt reading correctly so we the help of the ai i got it fixed

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+// eslint.config.js
+import js from '@eslint/js';
+import typescript from '@typescript-eslint/eslint-plugin';
+import typescriptParser from '@typescript-eslint/parser';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import prettier from 'eslint-plugin-prettier';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        console: true
+      }
+    },
+    rules: {
+      'prefer-const': 'error'
+    }
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      '@typescript-eslint': typescript,
+      react,
+      'react-hooks': reactHooks,
+      prettier
+    },
+    rules: {
+      'prefer-const': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn'
+    }
+  },
+  {
+    files: ['cypress/**/*.{js,jsx,ts,tsx}', 'cypress.config.js'],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        cy: true,
+        Cypress: true,
+        describe: true,
+        it: true,
+        expect: true,
+        beforeEach: true,
+        afterEach: true
+      }
+    },
+    rules: {
+      'no-unused-vars': 'off'
+    }
+  }
+];
